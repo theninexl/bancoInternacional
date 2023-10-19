51ac1a935ac6ec8550b09a9d853843f55ebc8523
theninexl <thenine@thenine.es>
2023-10-19 13:23:49 +0200

    add department data to edit user page and fix logo location

--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.png'
+import logo from '/images/logo.png'
 
 const Logo = () => {
   return (
--- src/pages/EditUser/index.jsx
+++ src/pages/EditUser/index.jsx
@@ -22,6 +22,7 @@ function EditUser(){
   //getUser
   const [userName, setUserName] = useState('');
   const [surName, setSurName] = useState('');
+  const [department, setDepartment] = useState('');
   const [email, setEmail] = useState('');
 
   const userID = searchParams.get('id');
@@ -35,6 +36,7 @@ function EditUser(){
     .then(response => {
       setUserName(response?.data?.name);
       setSurName(response?.data?.surname);
+      setDepartment(response?.data?.department);
       setEmail(response?.data?.email)
     }).catch(err => {
       setFormError(err);
@@ -59,6 +61,7 @@ function EditUser(){
     const data = {
       name: userName,
       surname: surName,
+      department: department,
       email: email,
       password: formData.get('UserPwd'),
       password2: formData.get('UserPwd2'),
@@ -69,6 +72,7 @@ function EditUser(){
         "id_user":userID,
         "name":data.name,
         "surname":data.surname,
+        "department":data.department,
         "email":data.email,
         "password":data.password,
         "cat_display":"",
@@ -121,6 +125,16 @@ function EditUser(){
                 value={surName}
                 handleOnChange={e => setSurName(e.target.value)}>
                   Apellidos
+              </LabelElement>
+            </SimpleFormRow>
+            <SimpleFormRow>
+              <LabelElement
+                htmlFor='UserDepartment'
+                type='text'
+                placeholder={department}
+                value={department}
+                handleOnChange={e => setDepartment(e.target.value)}>
+                  Departamento
               </LabelElement>
             </SimpleFormRow>
             <SimpleFormRow>
